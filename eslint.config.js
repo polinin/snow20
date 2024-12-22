6595eaeaabd84c6938496cc7a4ba40dfbbc824c0
polinin <polinarus@gmail.com>
2024-12-21 16:15:05 -0800

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,21 @@
 import pluginVue from 'eslint-plugin-vue';
 
 export default [
-    ...pluginVue.configs['flat/essential'],
-    {
-        rules: {
-            semi: 'error',
-            'prefer-const': 'error'
-        },
-        root: true,
-        env: {
-            'node': true
-        },
-        extends: [
-            'eslint:recommended'
-        ],
-        parser: '@babel/eslint-parser',
-        parserOptions: {
-            'sourceType': 'module',
-            'allowImportExportEverywhere': true
-        }
-    }
+  ...pluginVue.configs['flat/essential'],
+  {
+    rules: {
+      semi: 'error',
+      'prefer-const': 'error',
+    },
+    root: true,
+    env: {
+      node: true,
+    },
+    extends: ['eslint:recommended'],
+    parser: '@babel/eslint-parser',
+    parserOptions: {
+      sourceType: 'module',
+      allowImportExportEverywhere: true,
+    },
+  },
 ];
